c98cfaafc070afce254d92a2a554c5b64093f988
Thibault Mazan <tmazan@bess-f4r5s9.clusters.42paris.fr>
2024-05-14 16:43:43 +0200

    commit ex20

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,18 +6,41 @@
 /*   By: tmazan <tmazan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:44:45 by tmazan            #+#    #+#             */
-/*   Updated: 2024/05/14 15:58:46 by tmazan           ###   ########.fr       */
+/*   Updated: 2024/05/14 16:43:19 by tmazan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdlib.h>
+#include<stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return (i);
+}
 
 char *ft_strdup(char *src)
 {
-    char *dest;
-    
-    dest = (char *)malloc(ft_strlen(src + 1) sizeof(char));
+    char    *dest;
+    int     i;
+    dest = (char *)malloc(ft_strlen(src + 1) * sizeof(char));
     if (dest == NULL)
-        return ERROR_CODE;
-    
+        return ("error");
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return(dest);
+}
+
+int main(void)
+{
+    printf("%s\n", ft_strdup("Salut a tous !"));
 }
