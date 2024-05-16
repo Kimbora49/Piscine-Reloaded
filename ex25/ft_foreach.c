7357eb5bfd215320b28c820062eee336a3a8654f
Thibault Mazan <tmazan@paul-f4Ar4s5.clusters.42paris.fr>
2024-05-16 11:52:38 +0200

    commit ex25

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -5,17 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmazan <tmazan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/15 18:27:48 by tmazan            #+#    #+#             */
-/*   Updated: 2024/05/15 19:04:49 by tmazan           ###   ########.fr       */
+/*   Created: 2024/05/16 11:23:23 by tmazan            #+#    #+#             */
+/*   Updated: 2024/05/16 11:47:22 by tmazan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// #include <unistd.h>
+
+// void	ft_putchar(char c)
+// {
+// 	write (1, &c, 1);
+// }
+
+// void	ft_putnbr(int nb)
+// {
+// 	if (nb < 0)
+// 	{
+// 		nb = -nb;
+// 	}
+// 	if (nb >= 10)
+// 	{
+// 		ft_putnbr(nb / 10);
+// 		ft_putnbr(nb % 10);
+// 	}
+// 	else
+// 		ft_putchar(nb + '0');
+// }
+
 void ft_foreach(int *tab, int length, void (*f)(int))
 {
-
+    int i;
+    
+    i = 0;
+    while (i < length)
+    {
+        f(tab[i]);
+        i++;
+    }
 }
 
-int main(void)
-{
-    ft_foreach(tab, 1337, &ft_putnbr);
-}
+// int main(void)
+// {
+//     int tab[5] = {5, 10, 74, 42, 999};
+//     ft_foreach(tab, 5, &ft_putnbr);
+//     return (0);
+// }
